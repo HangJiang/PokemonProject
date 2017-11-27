5b15f45498077d1b7224435315d545ff7821416a
HangJiang <hitjh2012@163.com>
2017-11-27 14:50:58 +0800

    add dex.api

---
+++ src/Services/Dex/Dex.API/Controllers/PokemonsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dex.API.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Pokemons")]
+    public class PokemonsController : Controller
+    {
+        [HttpGet]
+        public string GetPokemons()
+        {
+            return "GetPokemons";
+        }
+
+        [HttpGet("{id}")]
+        public string GetPokemonByID(string id)
+        {
+            return "GetPokemonByID" + id;
+        }
+    }
+}
