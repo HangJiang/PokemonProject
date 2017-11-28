673b2c89de6b4d29e50fef29e1b70dda8c1f944a
HangJiang <hitjh2012@163.com>
2017-11-28 22:45:20 +0800

    update

---
+++ src/Services/Dex/Dex.API/Infrastructure/Repositories/IPokemonsRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dex.API.Infrastructure.Repositories
+{
+    public interface IPokemonsRepository
+    {
+        List<Object> GetPokemonsList();
+        Object GetPokemonInfo(string pmid);
+    }
+}
---
+++ src/Services/Dex/Dex.API/Infrastructure/Repositories/PokemonsRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dex.API.Infrastructure.Repositories
+{
+    public class PokemonsRepository : IPokemonsRepository
+    {
+        public object GetPokemonInfo(string pmid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<object> GetPokemonsList()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
